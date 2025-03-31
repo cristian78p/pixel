98e10357569ce583aa4645b1faf1afbafb3d31c5
cristian78p <ccreyb@gmail.com>
2025-03-31 00:03:12 -0500

    agregando el proyecto de visual studio

---
+++ Red Social Voluntarios/Red Social Voluntarios/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Red_Social_Voluntarios.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
