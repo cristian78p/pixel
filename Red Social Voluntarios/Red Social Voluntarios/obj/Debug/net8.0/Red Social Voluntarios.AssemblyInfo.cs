06ec4e40d683e6f4daa33da6145ce99d221b6d9d
CamiloCano <CamiloCano@DESKTOP-92TVVC5>
2025-05-14 16:45:41 -0500

    diseño_login

--- Red Social Voluntarios/Red Social Voluntarios/obj/Debug/net8.0/Red Social Voluntarios.AssemblyInfo.cs
+++ Red Social Voluntarios/Red Social Voluntarios/obj/Debug/net8.0/Red Social Voluntarios.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Red Social Voluntarios")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7dae74da216b6f737a3f56fc42f8d254ab6aeca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4bed4af7feadd81eb22252667fd667a1a1093e50")]
 [assembly: System.Reflection.AssemblyProductAttribute("Red Social Voluntarios")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Red Social Voluntarios")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
